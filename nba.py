c7b4cc06cf7a54090708f2677e3316f4de08fa7b
brighamaz <brighamaz@gmail.com>
2019-03-15 23:51:56 -0600

    Update nba.py
    I updated the code to use the full nbaRaw.csv dataset and included a little section at the bottom to calculate accuracy based on the distance between predicted playoff result and true playoff result. 
    
    So far the basic accuracy is consistently 60% and the distance accuracy is 87%
    
    We still need to normalize the data by year compared to the league average and account for the many different missing data sections.

--- nba.py
+++ nba.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Import all seasons for the Utah Jazz, Denver Nuggets, and Portland Trail Blazers
+Import all seasons for the entire NBA
 and attempt to predict playoff success using a MLP Classifier
 """
 
@@ -12,33 +12,37 @@ from sklearn import preprocessing
 
 
 #################### IMPORT DATASET ##############################
-data = pd.read_csv('nba.csv', 
+data = pd.read_csv('nbaRaw.csv', 
                    index_col=False,
                    skipinitialspace=True,
                    na_values="nan"
                    )
 # fill in missing 3pt data
-data.fillna(0.5, inplace=True)
+data.fillna(0, inplace=True)
 og_data = data.copy()
 
 # get the columns we're interested in
-real_data = data.drop(columns=['Season','Lg','Tm','Ht.','Finish','Wt.','G','MP','PLAYOFFS'])
-targets = data['PLAYOFFS']
+real_data = data.drop(columns=['Season','Lg','Tm','Finish','Wt.','Playoffs'])
+targets = data['Playoffs']
 
 X = real_data.values
 y = targets.values
-
 
 scaler = preprocessing.StandardScaler()
 X = scaler.fit_transform(X,y)
 
 ##################### TRAINING ####################################
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 mlp = MLPClassifier(hidden_layer_sizes=(30,), learning_rate_init=0.01, max_iter=10000)
 mlp.fit(X_train, y_train)
 
-
 #################### PREDICTION ###################################
 predictions = mlp.predict(X_test)
 print(f"Accuracy is {accuracy_score(y_test, predictions)}")
+inaccuracy= 0 
+for x in range(len(predictions)):
+    if predictions[x] != y_test[x]:
+        inaccuracy += abs(predictions[x]-y_test[x])
+        #print("predicted ", predictions[x], "Acutally", y_test[x])
+print("Distance Accuracy is", 1-(inaccuracy/(len(predictions)*5)))
