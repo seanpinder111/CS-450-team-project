1b616866427b8af36d1b988e5a885e5268a3e099
brighamaz <brighamaz@gmail.com>
2019-03-22 16:09:24 -0600

    Update nba.py

--- nba.py
+++ nba.py
@@ -7,6 +7,7 @@ and attempt to predict playoff success using a MLP Classifier
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
@@ -30,7 +31,7 @@ og_data = data.copy()
 
 # get the columns we're interested in
 real_data = data.drop(columns=['Season','Lg','Tm','Playoffs','year'])
-real_thisYear = thisYear.drop(columns=['Season','Lg','Tm','year'])
+real_thisYear = thisYear.drop(columns=['Season','Lg','Tm','Year'])
 teams= thisYear['Tm']
 targets = data['Playoffs']
 
@@ -46,6 +47,9 @@ mlp.fit(X_train, y_train)
 
 gnb = GaussianNB()
 gnbpredict= gnb.fit(X_train, y_train).predict(X_test)
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+knnpredict= neigh.fit(X, y).predict(X_test)
 
 #################### PREDICTION ###################################
 
@@ -70,11 +74,22 @@ for x in range(len(gnbpredict)):
 print("Distance Accuracy is", 1-(inaccuracy/(len(gnbpredict)*5)))
 
 
+print("\n\K Nearest Neighbors RESULTS")
+print(f"Accuracy is {accuracy_score(y_test, knnpredict)}")
+inaccuracy= 0 
+for x in range(len(knnpredict)):
+    if knnpredict[x] != y_test[x]:
+        inaccuracy += abs(knnpredict[x]-y_test[x])
+        #print("predicted ", predictions[x], "Acutally", y_test[x])
+print("Distance Accuracy is", 1-(inaccuracy/(len(knnpredict)*5)))
+
+
 
 # Predict for this year
 print("\n\nPredictions for 2019 Playoffs")
-print("\nTEAM: NN,NB")
+print("\nTEAM: NN,NB,KNN")
 playoffs1= mlp.predict(real_thisYear)
 playoffs2= gnb.predict(real_thisYear)
+playoffs3= neigh.predict(real_thisYear)
 for y in range(len(teams)):
-    print(teams[y], ":", playoffs1[y], ",", playoffs2[y])
+    print(teams[y], ":", playoffs1[y], ",", playoffs2[y], ",", playoffs3[y])
